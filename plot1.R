2febcc2c093e79ceaff7cabd5ba4d605578ef223
reems <reem.sarrif@aramex.com>
2015-03-08 23:46:06 +0200

    final set updated

--- plot1.R
+++ plot1.R
@@ -10,7 +10,7 @@ plot1 <- function() {
   
   par(mfrow=c(1,1))
   
-  hist(table(hpc$Global_active_power), 
+  hist(hpc$Global_active_power, 
        col="red",
        main="Global Active Power", 
        xlab="Global Active Power (kilowatts)",
