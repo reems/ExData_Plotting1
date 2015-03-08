a187de463f3b1f7d3ffad4e5c51774071d3a0899
reems <reem.sarrif@aramex.com>
2015-03-08 22:29:36 +0200

    final set

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-plot3 <- function() {
+plot4 <- function() {
   
   hpc <- read.table("./data/household_power_consumption.txt", sep=";",header=TRUE)
   hpc$Date <- as.Date(hpc$Date,format="%d/%m/%Y")
@@ -7,14 +7,17 @@ plot3 <- function() {
   hpc <- subset(hpc, Date %in% c(D1,D2))
   
   hpc$Global_active_power <- as.numeric(as.character(hpc$Global_active_power))
-  
+  hpc$Global_reactive_power <- as.numeric(as.character(hpc$Global_reactive_power))
   hpc$Sub_metering_1 <- as.numeric(as.character(hpc$Sub_metering_1))
   hpc$Sub_metering_2 <- as.numeric(as.character(hpc$Sub_metering_2))
   hpc$Sub_metering_3 <- as.numeric(as.character(hpc$Sub_metering_3))
   
   hpc$DateTime <- strptime(as.character(paste(hpc$Date,hpc$Time)), format="%Y-%m-%d %H:%M:%S")
   
-  par(mfrow=c(1,1))
+  par(mfrow=c(2,2))
+  
+  plot(hpc$DateTime, hpc$Global_active_power, type="l", ylab="Global Active Power(kilowatts)",xlab="")
+  plot(hpc$DateTime, hpc$Voltage, type="l", xlab="datetime", ylab="Voltage")
   
   plot(hpc$DateTime, hpc$Sub_metering_1, 
        type="l", 
@@ -26,7 +29,9 @@ plot3 <- function() {
   
   legend("topright", pch="__", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
   
-  dev.copy(png,"plot3.png")
+  plot(hpc$DateTime, hpc$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+  
+  dev.copy(png,"plot4.png")
   dev.off()
   
 }
